c5141695c6f48ec5abd651e99f29a2548a3616f5
Pi Lu <paulhendi25@gmail.com>
2022-07-21 16:28:56 +0200

    Update ESDTSender.py

--- ESDTSender.py
+++ ESDTSender.py
@@ -33,11 +33,11 @@ eligible_holders = data_df[data_df.Address.apply(lambda x: "qqqqqq" not in x)]
 
 # Compute the total of LKMEX per address (not taking into account the number of NFT hold)
 # TMP FIX : Remove 0.1 LKMEX per holder to avoid "insufficient founds" (due to Python's loss of precision)
-airdrop_per_holder = float(args.amount_airdrop) / ( eligible_holders.shape[0]) - 0.1  
+airdrop_per_holder = float(args.amount_airdrop) / ( eligible_holders.shape[0]) - 0.0001  
 
 # Compute the weighted airdrop if set to true as an argument
 if args.weighted : 
-    airdrop_per_NFT = float(args.amount_airdrop) / ( eligible_holders.Count.sum() ) - 0.1      
+    airdrop_per_NFT = float(args.amount_airdrop) / ( eligible_holders.Count.sum() ) - 0.0001      
     eligible_holders["Airdrop"] = airdrop_per_NFT*data_df.Count
 
 
