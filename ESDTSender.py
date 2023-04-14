3af418b1147bee5c5fcf799e5fcaa5ed5dd567c0
PABourdais <pierre.alex.bourdais@gmail.com>
2023-04-14 11:40:15 +0200

    refactor(ESDTSender.py): remove sdk cli usage

--- ESDTSender.py
+++ ESDTSender.py
@@ -2,7 +2,6 @@ from multiversx_sdk_wallet import UserSigner, UserPEM
 from multiversx_sdk_core import Address, TokenPayment
 from multiversx_sdk_core.transaction_builders import ESDTTransferBuilder, DefaultTransactionBuildersConfiguration
 from multiversx_sdk_network_providers import ProxyNetworkProvider
-from multiversx_sdk_cli.accounts import Account
 
 from pathlib import Path
 
@@ -61,25 +60,15 @@ PROXY = config_network[CHAIN]["proxy"]
 TOKEN_DECIMALS = args.decimals  # default : 18
 TOKEN_ID = args.id
 
-wallet = Account(pem_file=Path(f"./{args.pem}")).address
+# ---------------------------------------------------------------- #
+#                         CHECK ESDT EXISTENCE
+# ---------------------------------------------------------------- #
 
 try:
     response = requests.get(f'https://{PROXY}api.multiversx.com/tokens/{TOKEN_ID}')
     response.raise_for_status()
 except requests.exceptions.HTTPError as e:
     print(f"ERROR: Token ({TOKEN_ID}) does not exist")
-    sys.exit()
-
-try:
-    response = requests.get(f'https://{PROXY}api.multiversx.com/accounts/{wallet}/tokens/{TOKEN_ID}')
-    response.raise_for_status()
-    balance = response.json()['balance']
-    amount_to_drop = float(args.amount_airdrop) * pow(10, int(args.decimals))
-    if float(balance) < amount_to_drop:
-        print(f"ERROR: You don't have enough {TOKEN_ID}")
-        sys.exit()
-except requests.exceptions.HTTPError as e:
-    print(f"ERROR: You don't own any {TOKEN_ID}")
     sys.exit()
 
 # ---------------------------------------------------------------- #
@@ -116,6 +105,22 @@ owner = Address(pubkey, "erd")
 
 provider = ProxyNetworkProvider(f"https://{PROXY}gateway.multiversx.com")
 owner_on_network = provider.get_account(owner)
+
+# ---------------------------------------------------------------- #
+#                  CHECK ESDT BALANCE
+# ---------------------------------------------------------------- #
+
+try:
+    response = requests.get(f'https://{PROXY}api.multiversx.com/accounts/{owner}/tokens/{TOKEN_ID}')
+    response.raise_for_status()
+    balance = response.json()['balance']
+    amount_to_drop = float(args.amount_airdrop) * pow(10, int(args.decimals))
+    if float(balance) < amount_to_drop:
+        print(f"ERROR: You don't have enough {TOKEN_ID}")
+        sys.exit()
+except requests.exceptions.HTTPError as e:
+    print(f"ERROR: You don't own any {TOKEN_ID}")
+    sys.exit()
 
 # ---------------------------------------------------------------- #
 #                     AIRDROP LOOP
